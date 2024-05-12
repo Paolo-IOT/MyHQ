db4fa5a2267720f132e92e2e29c8746e282e16f8
Paolo_IOT <paolo.martinez1@gmail.com>
2024-05-12 08:05:45 +0200

    Delete hqmanager directory

--- hqmanager/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Event, Objective
-
-
-admin.site.register(Event)
-
-admin.site.register(Objective)
-
--- hqmanager/templatetags/filtri.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_bootstrap_class')
-def add_bootstrap_class(value, arg):
-    return value.as_widget(attrs={'class': arg})
